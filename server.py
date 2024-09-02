44e14f5413bee1feeaf12e93d234f6120f63660e
supersid2001 <supersid2001@gmail.com>
2024-09-01 22:10:04 -0400

    Edited tech notest

--- server.py
+++ server.py
@@ -52,7 +52,7 @@ web_projects = {
         "Platform": "Web",
         "image": "Notest.png",
         "Summary": "Notests is a collaborative project with two other JHU students, enhancing e-learning through structured study plans, community sharing, and AI-driven question generation for seamless self-testing.",
-        "Technologies": ["React.js", "Next.js", "MongoDB"],
+        "Technologies": ["Next.js", "CSS", "MongoDB"],
         "Repo": "https://github.com/robertzhidealx/notest",
         "Link": "https://youtu.be/VEOxllDq3tE"
     }
